b224f799e05a9db8d777a3653351bbc11c8336a4
coderEdu <educoder78@gmail.com>
2024-02-19 16:36:08 -0300

    Little change

--- AppUnidad3/Modelo/Cliente.cs
+++ AppUnidad3/Modelo/Cliente.cs
@@ -9,9 +9,9 @@ namespace AppUnidad3.Modelo
     public class Cliente
     {
         public int ID { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string Nacionalidad { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public string? Nacionalidad { get; set; }
 
     }
 }
